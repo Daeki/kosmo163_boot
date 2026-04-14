6143a0d7b0a0b30ed9c79d362620ab0f7e073030
Daeki <kimdaeki@gmail.com>
2026-04-14 18:29:32 +0900

    file8

--- src/main/java/com/winter/app/board/notice/NoticeDTO.java
+++ src/main/java/com/winter/app/board/notice/NoticeDTO.java
@@ -3,19 +3,16 @@ package com.winter.app.board.notice;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.winter.app.board.BoardDTO;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class NoticeDTO {
-	private Long boardNum;
-	private String boardTitle;
-	private String boardContents;
-	private String boardWriter;
-	private LocalDateTime boardDate;
-	private Long boardHit;
+public class NoticeDTO extends BoardDTO {
+
 	
 	private List<NoticeFileDTO> list;
 
